8accebf4f2f7a6e98fa7638283dcec92fbe853f6
Kion <ar-hangel@inbox.ru>
2025-02-21 13:07:01 +0400

    button

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -9,6 +9,7 @@ const WorksPage = () => <div>Страница с проектами</div>
 function App() {
   // внутри компонента (внутри App) создаем саму переменную, функцию для изменения
   // внутри скобок - начальное значение
+  
   const [counter, setCounter] = useState(0)
 
   return (
@@ -18,6 +19,7 @@ function App() {
      {counter}
      {/* на клик кнопки вызываем увеличение на 1 */}
      <button onClick={() => setCounter(counter + 1)}>+1</button>
+     <button onClick={() => setCounter(counter - 1)}>-1</button>
     </>
   )
 }
